302cfc475c230b73f360fd16e99fb2582681343a
MartenDriesen <113995903+MartenDriesen@users.noreply.github.com>
2023-03-16 17:22:03 +0100

    ready

--- js/index.js
+++ js/index.js
@@ -1,16 +1,67 @@
 "use strict";
 
 const chat = {
-    author: "yourName",
+    author: "Smewt",
     init() {
+        this.fetchMessages();
+        document.getElementById("chatForm").addEventListener("submit", this.sendMessage);
+
     },
-    sendMessage() {
+    sendMessage(event) {
+        event.preventDefault();
+        let newmessage = document.getElementById("chatInput").value;
+
+        const data = {
+            author: chat.author,
+            message: newmessage
+        };
+        console.log(newmessage);
+
+        fetch('https://dev2chat.onrender.com/message', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(newmessage),
+
+        });
+        document.querySelector('#messageContainer').innerHTML = "";
+        chat.fetchMessages();
+
     },
     fetchMessages() {
+        const result = fetch('https://dev2chat.onrender.com/messages')
+            .then(function (response) {
+                return response.json();
+
+            })
+            .then(function (messages) {
+                console.log(messages);
+                document.querySelector('#messageContainer').innerHTML = messages[0].message;
+                //  console.log(document.querySelector('#messageContainer').innerHTML);
+
+                messages.forEach(function (message) {
+
+                    const htmlmessage =
+                        `
+                    <div class="messageItem">
+                    <div class="header">
+                        <span class="${message.author}">TEST</span>
+                        <span class="time">00:00</span>
+                    </div>
+                    <p>
+                    ${message.message}
+                    </p>
+                 </div>
+                 `
+                        ;
+                    document.querySelector("#messageContainer").insertAdjacentHTML('beforeend', htmlmessage);
+
+                });
+            });
     },
     renderMessage(message) {
     }
 
-}
+
+};
 
 chat.init();
